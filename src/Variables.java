be9ac7c75433a7d239dfcbc3ca23fe5e8b009bb2
Hkhan023 <106184816+Hkhan023@users.noreply.github.com>
2022-05-26 14:36:41 -0400

    Update Variables.java

--- src/Variables.java
+++ src/Variables.java
@@ -1,8 +1,8 @@
 /**
  * class name: Variables.java
- * author: ________
- * date:
- * version:
+ * author: Hajira Khan
+ * date: 5/26/2022
+ * version: 1
  * description: This program will display summary of primitive data types.
  * source: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
  * direction: 1) read the source material
@@ -15,6 +15,14 @@ public class Variables
     public static void main(String[] args)
     {
         System.out.println("Summary of The Primitive Data Types");
+        System.out.println("Byte: Byte data type is for saving memory and could take in place of int.");
+        System.out.println("short: Short data type is Also to save space.");
+        System.out.println("int: Int means integer which is for numbaric values.");
+        System.out.println("long: Long data type is for when you need a range of values that are long.");
+        System.out.println("float: Float is when when the range of val".);
+        System.out.println("double: Double data type is when ");
+        System.out.println("boolean:"Boolean is used for true and false");
+        System.out.println("char: Char data types means its a character");
     }
 
 }
